53c0fde426d1e1b1ceb6ee6c974ac4131b33997a
GuilhermePie <guilhermeSpiedade@gmail.com>
2023-12-15 19:50:58 -0300

    adição de butões para alteração do nome da lista de tarefas

--- task-list/scripts/listname.js
+++ task-list/scripts/listname.js
@@ -1,6 +1,50 @@
 const list1 = document.getElementById('list-1')
 const list2 = document.getElementById('list-2')
 const list3 = document.getElementById('list-3')
+const edit1 = document.getElementById('edit1')
+const edit2 = document.getElementById('edit2')
+const edit3 = document.getElementById('edit3')
+
+edit1.addEventListener('click', () => {
+        list1.select()
+        list1.removeAttribute('readonly')
+})
+
+document.addEventListener('mousedown', (event) =>{
+    if(list1.contains(event.target)){
+
+    }else{
+        list1.setAttribute('readonly','')
+    }
+})
+
+edit2.addEventListener('click', () => {
+    list2.select()
+    list2.removeAttribute('readonly')
+})
+
+document.addEventListener('mousedown', (event) =>{
+if(list2.contains(event.target)){
+
+}else{
+    list2.setAttribute('readonly','')
+}
+})
+
+edit3.addEventListener('click', () => {
+    list3.select()
+    list3.removeAttribute('readonly')
+})
+
+document.addEventListener('mousedown', (event) =>{
+if(list3.contains(event.target)){
+
+}else{
+    list3.setAttribute('readonly','')
+}
+})
+
+// LISTAS
 
 if(localStorage.lista1){
     list1.value = localStorage.lista1
